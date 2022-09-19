a7cf859428f6e0b057ac7588cefe628ccd2e3e2c
mohammedahmed18 <mohammed18200118@gmail.com>
2022-09-19 14:05:05 +0200

    fix some issues

--- src/app/academic/term.pipe.ts
+++ src/app/academic/term.pipe.ts
@@ -7,6 +7,7 @@ export class TermPipe implements PipeTransform {
 
   transform(arr: any, code: any): any 
   {
+    
     if ( code == '' || code == null || code == undefined || code == '0' || arr == [] || arr == undefined || arr == null)
      {
       return arr;
