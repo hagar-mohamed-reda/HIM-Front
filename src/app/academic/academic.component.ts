52aacacb635f2efc228733e12a3824ca740f23ec
AMoraby3 <01205082343ahmed@gmail.com>
2022-09-19 10:46:51 +0200

    student101

--- src/app/academic/academic.component.ts
+++ src/app/academic/academic.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./academic.component.scss']
 })
 export class AcademicComponent implements OnInit {
-
+  public $: any = $;
+  public doc: any = document;
   constructor() { }
 
   ngOnInit() {
+    var self = this;
+    setTimeout(() => {
+      let height = window.innerHeight - 60;
+      self.$('.student-affair-container').css('height', height+"px");
+      //
+      console.log(height);
+    }, 500);
   }
 
 }
